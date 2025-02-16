98920c20a35fade24cf12a44b5f921a4ef81edb1
Grant Rettke <grant@wisdomandwonder.com>
2025-02-15 21:28:53 -0600

    Minor refactoring.

--- initialism.el
+++ initialism.el
@@ -4,7 +4,7 @@
 
 ;; Author: Grant Rettke <grant@wisdomandwonder.com>
 ;; Maintainer: Grant Rettke <grant@wisdomandwonder.com>
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: abbrev
 ;; Homepage: https://github.com/grettke/initialism
